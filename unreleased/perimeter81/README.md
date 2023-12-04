3be9350b5fcc25a6ec885434ac81cc3110095a82
Chris Blyth <blythmeister@gmail.com>
2023-12-04 20:01:12 +0000

    Add perimeter81

---
+++ unreleased/perimeter81/README.md
@@ -0,0 +1,5 @@
+# <img src="https://raw.githubusercontent.com/BlythMeister/chocolatey-packages/master/icons/perimeter81.jpg" width="48" height="48"/> [perimeter81](https://chocolatey.org/packages/perimeter81)
+
+Perimeter 81 client to connect to networks managed by Perimeter 81
+
+Delivering simpler, smarter, and more accessible network security to meet the demands of today, and the growth of tomorrow.
---
+++ unreleased/perimeter81/tools/chocolateyInstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$toolsPath      = Split-Path $MyInvocation.MyCommand.Definition
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  softwareName   = "Perimeter81*"
+  fileType       = 'msi'
+  url            = 'https://static.perimeter81.com/agents/windows/Perimeter81_10.3.1.1542.msi'
+  checksum       = 'e3e50e578a561ae9beb0b8dbf7cfe4517a60847cbe0e0ec72c3ce924f73609dc'
+  checksumType   = 'sha256'
+  silentArgs     = '/q /norestart /log `"$env:TEMP\chocolatey\$($env:ChocolateyPackageName)\$($env:ChocolateyPackageVersion)\Install.log`"'
+  validExitCodes = @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
