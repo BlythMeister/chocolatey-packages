ced906cfedcc448ca74ab69a2a167100c8709e4e
Chris Blyth <blythmeister@gmail.com>
2024-02-13 11:32:10 +0000

    Fix install args

--- automatic/perimeter81/tools/chocolateyInstall.ps1
+++ automatic/perimeter81/tools/chocolateyInstall.ps1
@@ -9,7 +9,7 @@ $packageArgs = @{
   url            = 'https://static.perimeter81.com/agents/windows/Perimeter81_10.4.2.1645.msi'
   checksum       = 'af37f79c904857ddf2687c5f011dfa2dd588de9d7aadfae85c29871658051418'
   checksumType   = 'sha256'
-  silentArgs     = '/q /norestart /log `"$env:TEMP\chocolatey\$($env:ChocolateyPackageName)\$($env:ChocolateyPackageVersion)\Install.log`"'
+  silentArgs     = '/q /norestart'
   validExitCodes = @(0)
 }
 
