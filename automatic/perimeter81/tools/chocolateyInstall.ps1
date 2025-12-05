6822844dbf2a1235733c0f0aeeb3623eaaf8beb6
Chris Blyth <blythmeister@gmail.com>
2025-12-05 21:16:07 +0000

    fix: convert perimeter81 to harmony meta
    - declare dependency on harmony-sase to turn package virtual
    
    - drop installer content and emit info message in chocolateyInstall
    
    - simplify update script to only track upstream version
    
    Signed-off-by: Copilot

--- automatic/perimeter81/tools/chocolateyInstall.ps1
+++ automatic/perimeter81/tools/chocolateyInstall.ps1
@@ -1,16 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$toolsPath      = Split-Path $MyInvocation.MyCommand.Definition
-
-$packageArgs = @{
-  packageName    = $env:ChocolateyPackageName
-  softwareName   = "Perimeter81*"
-  fileType       = 'msi'
-  url            = 'https://static.perimeter81.com/agents/windows/Harmony_SASE_12.1.0.9079.msi'
-  checksum       = '7dd280478775025506ddb9ce177b768765f35c1e3a7dab1574006b8aba3ff55a'
-  checksumType   = 'sha256'
-  silentArgs     = '/q /norestart'
-  validExitCodes = @(0)
-}
-
-Install-ChocolateyPackage @packageArgs
+Write-Host "Perimeter 81 has been renamed to Harmony SASE. This package only ensures the 'harmony-sase' dependency is installed."
