15f234c1264833ddfcc06c45bd782bbcf50b6cce
Chris Blyth <blythmeister@gmail.com>
2026-04-21 09:32:40 +0100

    feat: update harmoney-sase and perimeter81 to be sudo packages

--- automatic/harmony-sase/tools/chocolateyInstall.ps1
+++ automatic/harmony-sase/tools/chocolateyInstall.ps1
@@ -1,16 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$toolsPath      = Split-Path $MyInvocation.MyCommand.Definition
-
-$packageArgs = @{
-  packageName    = $env:ChocolateyPackageName
-  softwareName   = "Perimeter81*"
-  fileType       = 'msi'
-  url            = 'https://static.perimeter81.com/agents/windows/Harmony_SASE_12.7.4.14246.msi'
-  checksum       = '7b5fbf4d74b45f879428bc82f65296f8f67caaf3350f49b36f7bb029c8a2fbf9'
-  checksumType   = 'sha256'
-  silentArgs     = '/q /norestart'
-  validExitCodes = @(0)
-}
-
-Install-ChocolateyPackage @packageArgs
+Write-Host "Harmoney SASE has been renamed to Check Point SASE. This package only ensures the 'check-point-sase' dependency is installed."
