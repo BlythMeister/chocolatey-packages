ea637ac24dc8a81fc2a2ab508392533caa7a61e2
Chris Blyth <blythmeister@gmail.com>
2026-08-08 08:29:40 +0100

    Update update.ps1

--- automatic/perimeter81/update.ps1
+++ automatic/perimeter81/update.ps1
@@ -5,7 +5,19 @@ $versionPattern = [regex]"\d+\.\d+\.\d+\.\d+"
 $webHeaders = @{ 'User-Agent' = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AuScript' }
 
 function global:au_SearchReplace {
-  @{}
+  @{
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(?i)(^\s*fileType\s*=\s*)('.*')"       = "`$1'$($Latest.FileType)'"
+      "(?i)(^\s*url\s*=\s*)('.*')"            = "`$1'$($Latest.URL32)'"
+      "(?i)(^\s*checksum\s*=\s*)('.*')"       = "`$1'$($Latest.Checksum32)'"
+      "(?i)(^\s*checksumType\s*=\s*)('.*')"   = "`$1'$($Latest.ChecksumType32)'"
+    }
+  }
+}
+
+function global:au_BeforeUpdate {
+  $Latest.ChecksumType32 = 'sha256'
+  $Latest.Checksum32 = Get-RemoteChecksum -Algorithm $Latest.ChecksumType32 -Url $Latest.URL32
 }
 
 function Get-WindowsAgentVersion {
@@ -15,18 +27,22 @@ function Get-WindowsAgentVersion {
 
   $match = $versionPattern.Match($Content)
   if (-not $match.Success) {
-    throw "Unable to find 'Windows agent <version>' text on $releases"
+    throw "Unable to find version text on $releases"
   }
 
-  return Get-Version $match.Groups[1].Value
+  return Get-Version $match.Value
 }
 
 function global:au_GetLatest {
   $download_page = Invoke-WebRequest -Uri $releases -Headers $webHeaders
+
   $version = Get-WindowsAgentVersion -Content $download_page.Content
+  $downloadUrl = "$( $downloadBase )/Harmony_SASE_$( $version ).msi"
 
   return @{
+    URL32 = $downloadUrl
     Version = $version
+    FileType = "msi"
   }
 }
 
