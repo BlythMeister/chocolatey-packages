0a48ab2ca6e47c7c79f64c8aae404b1b0d013fd6
Chris Blyth <blythmeister@gmail.com>
2026-08-08 08:38:20 +0100

    Update chocolateyInstall.ps1

--- automatic/harmony-sase/tools/chocolateyInstall.ps1
+++ automatic/harmony-sase/tools/chocolateyInstall.ps1
@@ -1,16 +1,3 @@
-﻿$ErrorActionPreference = 'Stop'
+$ErrorActionPreference = 'Stop'
 
-$toolsPath      = Split-Path $MyInvocation.MyCommand.Definition
-
-$packageArgs = @{
-  packageName    = $env:ChocolateyPackageName
-  softwareName   = "Perimeter81*"
-  fileType       = 'msi'
-  url            = 'https://static.perimeter81.com/agents/windows/Harmony_SASE_12.10.0.16341.msi'
-  checksum       = 'dc0efab7327651899e0fe3e67ea12b99b92a5fa7028527941981e8d8a0efe47f'
-  checksumType   = 'sha256'
-  silentArgs     = '/q /norestart'
-  validExitCodes = @(0)
-}
-
-Install-ChocolateyPackage @packageArgs
+Write-Host "Harmony SASE has been renamed to Checkpoint SASE. This package only ensures the 'checkpoint-sase' dependency is installed."
