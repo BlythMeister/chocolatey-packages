04295961257df43fded96f2d38778106bc5160e2
Chris Blyth <blythmeister@gmail.com>
2026-08-08 08:27:14 +0100

    Update update.ps1

--- automatic/harmony-sase/update.ps1
+++ automatic/harmony-sase/update.ps1
@@ -1,8 +1,7 @@
 ﻿import-module au
 
-$releases = "https://support.perimeter81.com/docs/windows-agent-release-notes"
-$downloadBase = "https://static.perimeter81.com/agents/windows"
-$versionPattern = [regex]"Windows agent\s+(\d+\.\d+\.\d+\.\d+)"
+$releases = "https://sc1.checkpoint.com/documents/Infinity_Portal/WebAdminGuides/EN/SASE-Admin-Guide/Content/Topics-SASE-AG/Release-Notes/HS-Agent/Windows.htm"
+$versionPattern = [regex]"\d+\.\d+\.\d+\.\d+"
 $webHeaders = @{ 'User-Agent' = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AuScript' }
 
 function global:au_SearchReplace {
