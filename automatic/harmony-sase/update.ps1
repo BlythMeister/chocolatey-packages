399be528960d76b40713edbedd3d16b472e17cc4
Chris Blyth <blythmeister@gmail.com>
2026-08-18 20:58:57 +0100

    Update update.ps1

--- automatic/harmony-sase/update.ps1
+++ automatic/harmony-sase/update.ps1
@@ -1,25 +1,8 @@
 import-module au
 
-$downloadBase = "https://static.perimeter81.com/agents/windows"
 $releases = "https://sc1.checkpoint.com/documents/Infinity_Portal/WebAdminGuides/EN/SASE-Admin-Guide/Content/Topics-SASE-AG/Release-Notes/HS-Agent/Windows.htm"
 $versionPattern = [regex]"\d+\.\d+\.\d+\.\d+"
 $webHeaders = @{ 'User-Agent' = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AuScript' }
-
-function global:au_SearchReplace {
-  @{
-    ".\tools\chocolateyInstall.ps1" = @{
-      "(?i)(^\s*fileType\s*=\s*)('.*')"       = "`$1'$($Latest.FileType)'"
-      "(?i)(^\s*url\s*=\s*)('.*')"            = "`$1'$($Latest.URL32)'"
-      "(?i)(^\s*checksum\s*=\s*)('.*')"       = "`$1'$($Latest.Checksum32)'"
-      "(?i)(^\s*checksumType\s*=\s*)('.*')"   = "`$1'$($Latest.ChecksumType32)'"
-    }
-  }
-}
-
-function global:au_BeforeUpdate {
-  $Latest.ChecksumType32 = 'sha256'
-  $Latest.Checksum32 = Get-RemoteChecksum -Algorithm $Latest.ChecksumType32 -Url $Latest.URL32
-}
 
 function Get-WindowsAgentVersion {
   param(
@@ -38,12 +21,9 @@ function global:au_GetLatest {
   $download_page = Invoke-WebRequest -Uri $releases -Headers $webHeaders
 
   $version = Get-WindowsAgentVersion -Content $download_page.Content
-  $downloadUrl = "$( $downloadBase )/Harmony_SASE_$( $version ).msi"
 
   return @{
-    URL32 = $downloadUrl
     Version = $version
-    FileType = "msi"
   }
 }
 
